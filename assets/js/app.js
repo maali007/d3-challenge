ed9749b232601966cc4421c6c119fd70272a9a03
maali007 <maali007@umn.edu>
2021-06-16 14:10:15 -0500

    ReadingLoad and parse data.csv for poverty and healthcare variables.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
 // @TODO: YOUR CODE HERE!
+// Step 1: Using d3.csv, load data from data.csv
+d3.csv("assets/data/data.csv").then(function(stateStats) {
+
+  // Step 1.1: Parse the data to get poverty and healthcare values
+  stateStats.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+  });
+
+
+
+}).catch(function(error) {
+  console.log(error);
+});
+
